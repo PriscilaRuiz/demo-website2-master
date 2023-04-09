7951059bdbf0f9885d39053c5f40d3e408e1d3e9
Priscila Ruiz <priscilaruizbenavides@gmail.com>
2023-04-09 02:00:30 -0500

    Datos de conexion modificados

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 	function conexion(){
 	$host = "host=containers-us-west-35.railway.app";
-	$port = "port=66543";
+	$port = "port=6543";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
 	$password = "password=vcTnSUXTBqDJutECvjov";
