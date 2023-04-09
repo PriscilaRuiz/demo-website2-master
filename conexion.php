8b4e4153e6cc0c7fa2f258acb4635f7ec7666202
Priscila Ruiz <priscilaruizbenavides@gmail.com>
2023-04-09 01:57:36 -0500

    Datos de conexion

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 	function conexion(){
-	$host = "host=containers-us-west-41.railway.app";
-	$port = "port=6953";
+	$host = "host=containers-us-west-35.railway.app";
+	$port = "port=66543";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "password=gx48a5ALGCFUHSn4bfGb";
+	$password = "password=vcTnSUXTBqDJutECvjov";
 	$db = pg_connect("$host $port $dbname $user $password");
     return $db;
 }
