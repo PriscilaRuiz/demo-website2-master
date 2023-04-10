448c8e6c37c0f30afba83be88f1ce8a3f9302735
Priscila Ruiz <priscilaruizbenavides@gmail.com>
2023-04-10 01:53:48 -0500

    version10

--- listar.php
+++ listar.php
@@ -1,58 +1,52 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado Usuarios</title>
+</head>
+<body>
+    <div>
+        <h1 class="mt-5">Listado de Usuarios</h1>
+        <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Documento</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Dirección</th>
+                <th>Teléfono</th>
+            </tr>
+            <tbody>
+                <?php
+                    // Conexión a la base de datos
+                    include("conexion.php");
+                    $con = conexion();
 
-// Conexión a la base de datos
-include("conexion.php");
-$con = conexion();
+                    // Consulta a la base de datos
+                    $consulta = "SELECT documento, nombre, apellido, direccion, celular FROM persona";
+                    $resultados = pg_query($con, $consulta);
 
-// Consulta a la base de datos
-$consulta = "SELECT documento, nombre, apellido, direccion, celular FROM persona";
-$resultados = pg_query($con, $consulta);
+                    // Verificar si se obtuvieron resultados
+                    if (pg_num_rows($resultados) > 0) {
 
-// Verificar si se obtuvieron resultados
-if (pg_num_rows($resultados) > 0) {
-
-    if($obj=pg_fetch_object($resultados)){
-		echo '<table border="1">'.
-		'<caption>Usuario</caption>'.
-		'<tr>'.
-        '<th>Documento</th>'.
-		'<th>Nombre</th>'.
-		'<th>Apellido</th>'.
-		'<th>Direccion</th>'.
-		'<th>Celular</th>'.
-		'</tr>'.
-		'<tr>'.
-        '<th>'.$obj->documento.'</th>'.
-		'<th>'.$obj->nombre.'</th>'.
-		'<th>'.$obj->apellido.'</th>'.
-		'<th>'.$obj->direccion.'</th>'.
-		'<th>'.$obj->celular.'</th>'.
-		'</tr>'.
-		'</table>';
-    }
-}
-//     // Mostrar los registros en una tabla
-//     echo '<table>';
-//     echo '<tr>';
-//     echo '<th>Documento</th>';
-//     echo '<th>Nombre</th>';
-//     echo '<th>Apellido</th>';
-//     echo '<th>Direccion</th>';
-//     echo '<th>Celular</th>';
-
-//     while ($fila = pg_fetch_assoc($resultados)) {
-//         echo '<tr>';
-//         echo '<td>' . $fila['documento'] . '</td>';
-//         echo '<td>' . $fila['nombre'] . '</td>';
-//         echo '<td>' . $fila['apellido'] . '</td>';
-//         echo '<td>' . $fila['direccion'] . '</td>';
-//         echo '<td>' . $fila['celular'] . '</td>';
-//     }
-
-//     echo '</table>';
-// } else {
-//     echo 'No se encontraron registros.';
-// }
-header("location:listado.php");
-
-?>
+                        while ($fila = pg_fetch_assoc($resultados)) {
+                            echo '<tr>';
+                            echo '<td>' . $fila['documento'] . '</td>';
+                            echo '<td>' . $fila['nombre'] . '</td>';
+                            echo '<td>' . $fila['apellido'] . '</td>';
+                            echo '<td>' . $fila['direccion'] . '</td>';
+                            echo '<td>' . $fila['celular'] . '</td>';
+                            echo '</tr>';
+                        }
+                    } else {
+                        echo '<tr><td colspan="5">No se encontraron registros.</td></tr>';
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    
+</body>
+</html>
